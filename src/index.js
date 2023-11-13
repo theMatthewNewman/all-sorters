33966d81f4123aeea8efac3b6d6a2dcfc1099526
Matthew Newman <theMatthewNewman@gmail.com>
2023-11-13 10:55:41 -0500

    fixed bug

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function bubbleSort(n) {
+function bubbleSort(arr) {
     for (var i = 0; i < arr.length; i++) { 
         for (var j = 0; j < (arr.length - i - 1); j++) { 
             if (arr[j] > arr[j + 1]) { 
