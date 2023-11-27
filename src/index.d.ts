c1c6ddaa88ec0f054ea101a50fa5300d3c3cd330
Matthew Newman <theMatthewNewman@gmail.com>
2023-11-27 16:56:32 -0500

    new selection sort!

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,2 @@
 export declare function bubbleSort(n:number[]): number[];
+export declare function selectionSort(n:number[]): number[];
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 function bubbleSort(arr) {
-    for (var i = 0; i < arr.length; i++) { 
-        for (var j = 0; j < (arr.length - i - 1); j++) { 
+    for (let i = 0; i < arr.length; i++) { 
+        for (let j = 0; j < (arr.length - i - 1); j++) { 
             if (arr[j] > arr[j + 1]) { 
-                var temp = arr[j] 
+                let temp = arr[j] 
                 arr[j] = arr[j + 1] 
                 arr[j + 1] = temp 
             } 
@@ -11,4 +11,21 @@ function bubbleSort(arr) {
     return(arr); 
 }
 
+function selectionSort(arr)
+{
+    for (let i = 0; i < arr.length; i++)
+    {
+        let min_idx = i;
+        for (let j = i + 1; j < arr.length; j++)
+        if (arr[j] < arr[min_idx]) {
+            min_idx = j;
+        }
+        swap(arr,min_idx, i);
+        let temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
+
 exports.bubbleSort = bubbleSort;
+exports.selectionSort = selectionSort;
